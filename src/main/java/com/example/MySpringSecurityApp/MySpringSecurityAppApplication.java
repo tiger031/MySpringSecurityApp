36d5a7a4b2aa452b1068a6186918ca460dbc84ea
tiger031 <shelest100500@gmail.com>
2023-03-09 12:52:28 +0200

    Initial commit

---
+++ src/main/java/com/example/MySpringSecurityApp/MySpringSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MySpringSecurityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringSecurityAppApplication.class, args);
+	}
+
+}
